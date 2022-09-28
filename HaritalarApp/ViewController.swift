c8fd42a53a0e4e939e1d0b1919eca9c5d1788192
omerUnver <muhammedomerunver@gmail.com>
2022-09-28 15:46:59 +0300

    Initial Commit

---
+++ HaritalarApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaritalarApp
+//
+//  Created by M.Ömer Ünver on 28.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
